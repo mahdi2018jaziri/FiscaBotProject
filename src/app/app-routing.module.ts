7ef06d42686b3297da545c1a2664505dcb257e4e
Mahdi Jaziri <Mahdi.Jaziri@tn.ey.com>
2023-05-16 10:12:29 +0100

    Implement Page accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from 'src/LoginComponent/login/login.component';
 import { HomeComponent } from 'src/home/home.component';
-const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent }
+import { AuthGuardService } from 'src/auth-guard.service';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent   }
+ 
+  // canActivate: [AuthGuardService]
 
 ];
 
