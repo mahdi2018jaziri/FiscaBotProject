417af2c9e7351b155575d2d24d13e4b99826decb
WissalTa98 <twissal16@gmail.com>
2023-05-22 11:26:22 +0100

    Fixed Footer and addig scroll-button

--- Client/src/app/shared/scroll-button/scroll-button.component.ts
+++ Client/src/app/shared/scroll-button/scroll-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-scroll-button',
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class ScrollButtonComponent {
 
+  showButton: boolean = false;
+  readonly scrollOffset: number = 300;
+
+  @HostListener('window:scroll')
+  onScroll() {
+    this.showButton = window.pageYOffset > this.scrollOffset;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
 }
